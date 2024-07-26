9551f57a71f8c3a7401d22045e37c20ac699429f
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2024-07-26 17:04:15 +0300

    chore: Update mongoose dependency to version 8.5.1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 // middleware
 app.use(express.json());
@@ -13,4 +15,13 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-app.listen(port, console.log(`Server is running on port ${port}`));
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is running on port ${port}...`));
+  } catch (error) {
+    console.error("Error starting server:", error);
+  }
+};
+start();
--- routes/tasks.js
+++ routes/tasks.js
@@ -8,6 +8,12 @@ const {
   deleteTask,
 } = require("../controllers/tasks");
 
+/*
+Key Differences:
+Scope of update: PUT replaces the entire resource, PATCH modifies specific fields.
+Data sent: PUT requires all fields, PATCH only requires fields to be updated.
+*/
+
 router.route("/").get(getAllTasks).post(createTask);
 router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
